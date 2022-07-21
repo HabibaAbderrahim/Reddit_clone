ce6761c259239170d2c96363aaf80baee8a95fda
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-21 17:52:13 +0100

    push project structure

---
+++ reddit_clone/src/main/java/com/example/reddit_clone/RedditCloneApplication.java
@@ -0,0 +1,15 @@
+package com.example.reddit_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneApplication.class, args);
+
+		System.out.println("Hello words");
+	}
+
+}
