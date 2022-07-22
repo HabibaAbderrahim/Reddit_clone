1293a01cee54d5c3923e58a456adbd15531325a3
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-22 15:13:49 +0100

    user entity

---
+++ reddit_clone/src/main/java/com/example/reddit_clone/entities/User.java
@@ -0,0 +1,31 @@
+package com.example.reddit_clone.entities;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long userId ;
+    @NotNull
+    private String userName ;
+    @NotNull
+    private  String password ;
+    @NotNull
+    private String email ;
+    private Instant createDate ;
+}
