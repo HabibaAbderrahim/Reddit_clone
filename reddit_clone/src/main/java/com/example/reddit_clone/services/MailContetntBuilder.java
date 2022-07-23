d1112429ba5388940eca2f2432f5668ce689fe6c
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-23 18:10:05 +0100

    msg content to sent with mail

---
+++ reddit_clone/src/main/java/com/example/reddit_clone/services/MailContetntBuilder.java
@@ -0,0 +1,24 @@
+package com.example.reddit_clone.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailContetntBuilder {
+
+    @Autowired
+    private TemplateEngine templateEngine ;//thym
+
+    //email msg we want to sent to the user as input
+    String build(String message){
+
+        Context context = new Context();
+        context.setVariable("msg",message);
+        return templateEngine.process("mailTemplate",context);
+
+    }
+
+
+}
