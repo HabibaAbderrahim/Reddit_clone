3c1575b62dba2e31de826e4bddc4fabe348411b6
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-23 16:14:40 +0100

    BCryptPasswordEncoder

--- reddit_clone/src/main/java/com/example/reddit_clone/config/SecurityConfig.java
+++ reddit_clone/src/main/java/com/example/reddit_clone/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.reddit_clone.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -11,9 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/", "/login").permitAll()
+                .authorizeRequests().antMatchers("/", "Auth/login").permitAll()
                 .anyRequest().authenticated() ;
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 
