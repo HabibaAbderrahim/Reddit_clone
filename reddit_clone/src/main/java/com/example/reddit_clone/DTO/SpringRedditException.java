695692f935afaa9c1f8fe14b0580ac149353d00d
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-23 18:57:11 +0100

    add personal exception msg

---
+++ reddit_clone/src/main/java/com/example/reddit_clone/DTO/SpringRedditException.java
@@ -0,0 +1,17 @@
+package com.example.reddit_clone.DTO;
+
+
+import org.springframework.mail.MailException;
+
+//custom expections for user : like 404 ect ...
+//and they are common
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMsg, MailException e){
+        super(exMsg);
+
+
+    }
+
+
+
+}
