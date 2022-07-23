b6a89b453d9c61fe0554ed7421bd0d37cafdadf0
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-23 18:16:24 +0100

    add notification with attributes subject sender body

---
+++ reddit_clone/src/main/java/com/example/reddit_clone/entities/NotificationMail.java
@@ -0,0 +1,18 @@
+package com.example.reddit_clone.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class NotificationMail {
+    private String subject ;
+    private String sender ;
+    private String body ;
+}
