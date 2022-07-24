b6a2b2c8e8a4a3390aceabaceaef1801f9265527
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-24 16:22:24 +0100

    change userName to username

--- reddit_clone/src/main/java/com/example/reddit_clone/entities/User.java
+++ reddit_clone/src/main/java/com/example/reddit_clone/entities/User.java
@@ -22,7 +22,7 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long userId ;
     @NotNull
-    private String userName ;
+    private String username ;
     @NotNull
     private  String password ;
     @NotNull
