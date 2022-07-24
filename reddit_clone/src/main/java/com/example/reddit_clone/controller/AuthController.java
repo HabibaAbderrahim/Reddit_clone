da4d1970f69a7dd268495f6d631b7b933c3a2b99
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-24 16:18:30 +0100

    /api/authentification/signup controller

--- reddit_clone/src/main/java/com/example/reddit_clone/controller/AuthController.java
+++ reddit_clone/src/main/java/com/example/reddit_clone/controller/AuthController.java
@@ -6,22 +6,24 @@ import com.example.reddit_clone.services.auth.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/authentification")
+@RequestMapping("/api/auth")
+///api/auth/signup
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
         authService.signup(registerRequest);
-        return new ResponseEntity<String>("Success registration", HttpStatus.OK);
-
+        return new ResponseEntity<>("User Registration Successful",
+                HttpStatus.OK);
     }
 }
