f95133547e09ecd6a184801a6f5df1b60b9716f6
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-25 14:14:52 +0100

    add findByToken

--- reddit_clone/src/main/java/com/example/reddit_clone/repositories/VerifTokenRepository.java
+++ reddit_clone/src/main/java/com/example/reddit_clone/repositories/VerifTokenRepository.java
@@ -3,5 +3,9 @@ package com.example.reddit_clone.repositories;
 import com.example.reddit_clone.entities.VerficationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VerifTokenRepository extends JpaRepository<VerficationToken , Long> {
+
+    public Optional<VerficationToken> findByToken(String token);
 }
