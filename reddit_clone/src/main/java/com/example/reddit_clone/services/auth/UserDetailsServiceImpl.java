4ec2cb33428799281b31b62817bb435d26ca37f6
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-25 15:26:07 +0100

    impliment the UseDetailService Interface and GrantAuthority with role USER

---
+++ reddit_clone/src/main/java/com/example/reddit_clone/services/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.reddit_clone.services.auth;
+
+import com.example.reddit_clone.DTO.SpringRedditException;
+import com.example.reddit_clone.entities.User;
+import com.example.reddit_clone.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository ;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //find user
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        User user = userOptional
+                .orElseThrow(() -> new UsernameNotFoundException("No user " +
+                        "Found with username : " + username));
+
+        return new org.springframework.security
+                .core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isEnabled(), true, true,
+                true, getAuthories("USER"));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthories(String role) {
+
+        return  Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+}
