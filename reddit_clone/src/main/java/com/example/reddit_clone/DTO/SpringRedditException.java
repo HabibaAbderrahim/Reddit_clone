e4797af444ea99a55cbc53edb03f9cb02e7f1d4e
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-25 14:14:33 +0100

    update parameters

--- reddit_clone/src/main/java/com/example/reddit_clone/DTO/SpringRedditException.java
+++ reddit_clone/src/main/java/com/example/reddit_clone/DTO/SpringRedditException.java
@@ -6,7 +6,7 @@ import org.springframework.mail.MailException;
 //custom expections for user : like 404 ect ...
 //and they are common
 public class SpringRedditException extends RuntimeException {
-    public SpringRedditException(String exMsg, MailException e){
+    public SpringRedditException(String exMsg){
         super(exMsg);
 
 
