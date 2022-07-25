7f7eb1a051d2c004ceee1722a651297db045cf88
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-25 15:24:34 +0100

    AuthenticationManagerBuilder

--- reddit_clone/src/main/java/com/example/reddit_clone/config/SecurityConfig.java
+++ reddit_clone/src/main/java/com/example/reddit_clone/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.example.reddit_clone.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;//sec
 
 
     @Override
@@ -17,6 +25,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/", "/api/auth/**").permitAll()
                 .anyRequest().authenticated() ;
 
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
